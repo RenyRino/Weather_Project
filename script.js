9a50efccc473cb8bc3074a23920f2aba214e8ae0
reny <reny.rino@gmail.com>
2022-05-21 22:17:30 +0200

    Add forecast JS

--- script.js
+++ script.js
@@ -21,6 +21,35 @@ function formatDate(timestamp) {
   return `Last Updated: ${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  days.forEach(function (days) {
+    forecastHTML =
+      forecastHTML +
+      `
+     <div class="col-2">
+                <div class="forecast-date">Thu</div>
+                <div class="forecast-img">
+                  <img
+                    src="http://openweathermap.org/img/wn/01d@2x.png"
+                    alt=""
+                    width="42"
+                  />
+                </div>
+                <div class="forecast-temp">
+                  <span class="forecast-temp-max"> 18° </span>
+                  <span class="forecast-temp-min"> 12° </span>
+                </div>
+              </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+displayForecast();
+
 function displayTemp(response) {
   console.log(response.data);
   let cityElement = document.querySelector("#city");
